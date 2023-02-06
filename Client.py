44ea7a7dbc9b149de3c994943f45f9b827be7948
Danijel Dupor <danijeldupor.dd@gmail.com>
2023-02-06 21:13:04 +0100

    1.Commit

--- Client.py
+++ Client.py
@@ -1,1 +1,44 @@
 import asyncio
+import aiohttp
+import pandas as pd
+
+print("Pokretanje klijent skripte\n")
+
+listaIdKlijenata= list(range(1, 10001))
+print("Učitavanje podataka\n")
+dataframe= pd.read_json("data/dataset.json", lines=True)
+print("Podatci učitani.\n")
+
+redoviPoKlijentu= int(len(dataframe) / len(listaIdKlijenata))
+
+klijenti= {id:[] for id in listaIdKlijenata}
+
+for id, codes in klijenti.items():
+    odReda= (id-1)*redoviPoKlijentu
+    doReda= odReda+redoviPoKlijentu
+    for index, red in dataframe.iloc[odReda+1:doReda+1].iterrows():
+        codes.append(red.get("content"))
+        
+zadatci=[]
+rezultati=[]
+
+async def procesiranjeKoda():
+    global zadatci
+    global rezultati
+    
+    print("Slanje podataka\n")
+    async with aiohttp.ClientSession(connector= aiohttp.TCPConnector(ssl= False)) as session:
+        for id, codes in klijenti.items():
+            zadatci.append(asyncio.create_task(session.get("http://127.0.0.1:8080/", json= {"client":id, "codes":codes})))
+        print("Podatci poslani.\n")
+        print("Čekanje svih odgovora...\n")
+        rezultati= await asyncio.gather(*zadatci)
+        rezultati= [await x.json() for x in rezultati]
+        print("Dohvaćeni rezultati obrade podataka za sve klijente.\n")
+        
+asyncio.get_event_loop().run_until_complete(procesiranjeKoda())
+
+procesiraniPodatci= {}
+for rezultat in rezultati:
+    print("Prosječna duljina koda za klijenta koji ima ID", rezultat.get("client"), "je", rezultat.get("averageWordcount"))
+    
---
+++ Master.py
@@ -0,0 +1,74 @@
+from aiohttp import web
+import random
+import asyncio
+import aiohttp
+
+glavniPort= 8080
+
+X= 1000
+maxBrTrazenihOdgovora= 10000
+brPrimljenihZahtjeva= 0
+brVracenihOdgovora= 0
+brPoslanihZadataka= 0
+brIzvrsenihZadataka= 0
+
+Y= random.randint(5, 10)
+print("Number of workers:", Y)
+
+workers= {"workerWithId"+str(id): [] for id in range(1, Y+1)}
+print("Workers:", workers)
+
+routes= web.RouteTableDef()
+
+@routes.get("/")
+async def func(request):
+    try:
+        global Y, workers, X
+        global maxBrTrazenihOdgovora
+        global brPrimljenihZahtjeva, brVracenihOdgovora
+        global brPoslanihZadataka, brIzvrsenihZadataka
+        
+        brPrimljenihZahtjeva +=1
+        print(f"Novi zahtjev primljen. Trenutno stanje primljenih zahtjeva: {brPrimljenihZahtjeva} / {maxBrTrazenihOdgovora}")
+        podatci= await request.json()
+        duzinaKoda= len(podatci.get("codes"))
+        
+        sviKodovi= '\n'.join(podatci.get("codes"))
+        kodovi= sviKodovi.split("\n")
+        podatci["codes"]= ["\n".join(kodovi[i:i+X]) for i in range(0, len(kodovi), X)]
+        
+        zadatci= []
+        rezultati= []
+        async with aiohttp.ClientSession(connector= aiohttp.TCPConnector(ssl= False)) as  session:
+            trenutniWorker= 1
+            for i in range(len(podatci.get("codes"))):
+                zadatak= asyncio.create_task(session.get(f"http://127.0.0.1:{8080+trenutniWorker}/", json= {"id": podatci.get("client"), "podatci": podatci.get("codes")[i]}))
+                brPoslanihZadataka +=1
+                print(f"Novi zadatak poslan na worker {trenutniWorker}. Trenutno stanje poslanih zadataka: {brPoslanihZadataka}")
+                zadatci.append(zadatak)
+                workers["workerWithId"+str(trenutniWorker)].append(zadatak)
+                if trenutniWorker == Y:
+                    trenutniWorker= 1
+                else:
+                    trenutniWorker +=1
+                    
+            rezultati= await asyncio.gather(*zadatci)
+            brIzvrsenihZadataka += len(rezultati)
+            print(f"Još {len(rezultati)} zadataka je izvršeno. Trenutno stanje dovršenih zadataka: {brIzvrsenihZadataka}")
+            rezultati= [await rezultat.json() for rezultat in rezultati]
+            rezultati= [rezultat.get("numberOfWords") for rezultat in rezultati]
+            
+        brVracenihOdgovora += 1
+        print(f"Novi odgovor poslan. Trenutno stanje poslanih odgovora: {brVracenihOdgovora} / {maxBrTrazenihOdgovora}")   
+        
+        return web.json_response({"naziv": "master", "status": "OK", "klijent": podatci.get("client"), "prosječanBrRiječi": round(sum(rezultati) / duzinaKoda, 2)}, status = 200)
+        
+    except Exception as e:
+        return web.json_response({"naziv": "master", "error": str(e)}, status = 500)        
+
+
+
+
+app = web.Application()
+app.router.add_routes(routes)
+web.run_app(app, port = glavniPort, access_log = None)
